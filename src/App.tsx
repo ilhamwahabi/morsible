394d83cb81237fdb085d1d440d9f96ac2915e3bc
iwgx <wahabiputra@gmail.com>
2021-02-08 12:52:46 +0700

    Able to translate from 26 char alphabet to morse

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const textToMorse = (text: string): string => {
+  const mapper: { [key: string]: string } = {
+    'a':	'.-',	
+    'b':	'-...',	
+    'c':	'-.-.',	
+    'd':	'-..',	
+    'e':	'.',	
+    'f':	'..-.',
+    'g':	'--.',	
+    'h':	'....',	
+    'i':	'..',	
+    'j':	'.---',	
+    'k':	'-.-',	
+    'l':	'.-..',
+    'm':	'--',	
+    'n':	'-.',	
+    'o':	'---',	
+    'p':	'.--.',	
+    'q':	'--.-',	
+    'r':	'.-.',
+    's':	'...',	
+    't':	'-',	
+    'u':	'..-',	
+    'v':	'...-',	
+    'w':	'.--',	
+    'x':	'-..-',
+    'y':	'-.--',	
+    'z':	'--..',
+  }
+
+  let morseResult = '';
+
+  for (const char of text) {
+    if (mapper[char]) morseResult += mapper[char]
+  }
+  
+  return morseResult;
+}
 
 function App() {
+  const [text, setText] = useState('')
+  const [morse, setMorse] = useState('')
+
+  useEffect(() => {
+    setMorse(textToMorse(text))
+  }, [text])
+
   return (
     <div>
-      Semar
+      <header>
+        Semar
+      </header>
+      <main>
+        <input type="text" placeholder="Masukkan Teks" value={text} onChange={event => setText(event.target.value)} />
+        <input type="text" placeholder="Keluaran Kode Morse" disabled value={morse} onChange={event => setMorse(event.target.value)} />
+      </main>
     </div>
   )
 }
