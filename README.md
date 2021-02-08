11808ffb272c3428b37d0f29837a5279b6962d64
iwgx <wahabiputra@gmail.com>
2021-02-08 13:57:36 +0700

    Update README

--- README.md
+++ README.md
@@ -1,22 +1,5 @@
-# Over Hangman
-
-Translator for text/voice <-> morse code, but with high performance; pwa support; and accessibility friendly.
-
-Build for IF4081 - Informatika untuk Komunitas and also to explore many things about audio management in React.
-
-Tech stack:
-
-- React (hooks) + Typescript with Vite
-- jotai for state management
-- twin.macro for styling
-- Framer Motion for animation
-- react-testing-library for testing
-- Vercel for deployment
-
 ## Usage
 
 1. Clone this repo
 2. Install package, `npm install`
 3. Run, `npm run dev`
-
-Or open direcly here https://semar.iwgx.io
--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,18 @@
-import React, { useEffect, useState } from 'react'
-
-const textToMorse = (text: string): string => {
-  const mapper: { [key: string]: string } = {
-    'a':	'.-',	
-    'b':	'-...',	
-    'c':	'-.-.',	
-    'd':	'-..',	
-    'e':	'.',	
-    'f':	'..-.',
-    'g':	'--.',	
-    'h':	'....',	
-    'i':	'..',	
-    'j':	'.---',	
-    'k':	'-.-',	
-    'l':	'.-..',
-    'm':	'--',	
-    'n':	'-.',	
-    'o':	'---',	
-    'p':	'.--.',	
-    'q':	'--.-',	
-    'r':	'.-.',
-    's':	'...',	
-    't':	'-',	
-    'u':	'..-',	
-    'v':	'...-',	
-    'w':	'.--',	
-    'x':	'-..-',
-    'y':	'-.--',	
-    'z':	'--..',
-    '0': '-----',	
-    '1': '.----',	
-    "2": '..---',	
-    "3": '...--',	
-    "4": '....-',	
-    "5": '.....',
-    "6": '-....',	
-    "7": '--...',	
-    "8": '---..',	
-    "9": '----.',
-    ".": '.-.-.-',	
-    ",": '--..--',	
-    "?": '..--..',	
-    "'": '.----.',	
-    "!": '-.-.--',	
-    "/": '-..-.',
-    "(": '-.--.',	
-    ")": '-.--.-',	
-    "&": '.-...',	
-    ":": '---...',	
-    ";": '-.-.-.',	
-    "=": '-...-',
-    "+": '.-.-.',	
-    "-": '-....-',	
-    "_": '..--.-',	
-    '"': '.-..-.',	
-    "$": '...-..-',	
-    "@": '.--.-.',
-    "¿": '..-.-',	
-    "¡": '--...-',
-    " ": ' / '
-  }
-
-  let morseResult = '';
-
-  for (const char of text) {
-    if (mapper[char]) morseResult += mapper[char]
-  }
-  
-  return morseResult;
-}
+import React, { useState } from 'react'
+import 'twin.macro'
 
 function App() {
   const [text, setText] = useState('')
-  const [morse, setMorse] = useState('')
-
-  useEffect(() => {
-    setMorse(textToMorse(text))
-  }, [text])
 
   return (
     <div>
-      <header>
-        Semar
-      </header>
-      <main>
-        <input type="text" placeholder="Masukkan Teks" value={text} onChange={event => setText(event.target.value)} />
-        <input type="text" placeholder="Keluaran Kode Morse" disabled value={morse} onChange={event => setMorse(event.target.value)} />
-      </main>
+      <input
+        type="text"
+        placeholder="Masukkan Teks"
+        value={text}
+        tw="border"
+        onChange={event => setText(event.target.value)}
+      />
     </div>
   )
 }
