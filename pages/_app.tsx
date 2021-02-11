8ca6ed82974b18fa4e7812d9c2f6c5ca36933d64
iwgx <wahabiputra@gmail.com>
2021-02-12 03:34:07 +0700

    Move meta tag to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import { GlobalStyles } from 'twin.macro'
 import splitbee from '@splitbee/web';
+import Head from 'next/head';
 import 'typeface-roboto'
 
 splitbee.init()
 
 const App = ({ Component, pageProps }) => (
-  <div>
+  <>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="shortcut icon" href="favicon.ico" />
+      <title>Semar | Morse translator</title>
+    </Head>
     <GlobalStyles />
     <Component {...pageProps} />
-  </div>
+  </>
 )
 
 export default App
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { extractCritical } from '@emotion/server'
 
-export default class MyDocument extends Document<{ ids, css }> {
-  static async getInitialProps(ctx) {
+export default class MyDocument extends Document<{ ids: any, css: any }> {
+  static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx)
     const page = await ctx.renderPage()
     const styles = extractCritical(page.html)
@@ -13,10 +13,6 @@ export default class MyDocument extends Document<{ ids, css }> {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="shortcut icon" href="favicon.ico" />
-          <title>Semar | Morse translator</title>
           <style
             data-emotion-css={this.props.ids.join(' ')}
             dangerouslySetInnerHTML={{ __html: this.props.css }}
