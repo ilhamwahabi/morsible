52e55d0fa9acf0dc363fda8ddfb44fb923d7e42c
iwgx <wahabiputra@gmail.com>
2021-02-11 15:20:05 +0700

    Update basic styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,28 +8,30 @@ function App() {
   const [morse, setMorse] = useState('')
 
   return (
-    <div className="App__container">
-      <header className="App__header">
-        Semar
+    <div tw="container mx-auto py-8 px-8">
+      <header>
+        <h1 tw="text-4xl">Semar</h1>
       </header>
-      <main className="App__main">
-        <div>
+      <main tw="flex flex-col lg:flex-row lg:items-end justify-between mt-8">
+        <div tw="flex flex-col lg:w-5/12">
           <textarea
             placeholder="Masukkan Teks"
             value={text}
             rows={8}
+            tw="border border-gray-300 rounded-2xl resize-none p-4"
             onChange={event => {
               setText(event.target.value)
               setMorse(textToMorse(event.target.value))
             }}
           />
         </div>
-        <div tw="flex flex-col">
-          <button>Play</button>
+        <div tw="flex flex-col lg:w-5/12 mt-10 lg:mt-0">
+          <button tw="border bg-blue-500 text-white rounded-xl w-min px-8 py-2 mb-4 lg:mb-8">Play</button>
           <textarea
             placeholder="Kode Morse"
             value={morse}
             rows={8}
+            tw="border border-gray-300 rounded-2xl resize-none p-4"
             onChange={event => {
               setMorse(event.target.value)
               setText(morseToText(event.target.value))
