7073b2c50596dac38a1c58ef3acf0019a0f97c1f
iwgx <wahabiputra@gmail.com>
2021-03-03 10:55:15 +0700

    Add icon to morse player button and simplify naming

--- components/MorsePlayer.tsx
+++ components/MorsePlayer.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import tw from 'twin.macro'
 import useSound from 'use-sound'
+import { FaPlay, FaStop } from "react-icons/fa";
 
 function timeout(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -11,14 +12,14 @@ interface IProps {
 }
 
 function MorsePlayer({ morse }: IProps) {
-  const [isPlayingMorse, setIsPlayingMorse] = useState({ status: false })
+  const [isPlaying, setIsPlaying] = useState({ status: false })
 
   const [playDot, dotData] = useSound('/dot.mp3');
   const [playDash, dashData] = useSound('/dash.mp3');
 
-  const playMorse = async () => {
+  const play = async () => {
     let charIndex = 0
-    while (charIndex < morse.length && isPlayingMorse.status) {
+    while (charIndex < morse.length && isPlaying.status) {
       const char = morse[charIndex]
 
       if (char === ".") {
@@ -36,30 +37,33 @@ function MorsePlayer({ morse }: IProps) {
       charIndex++
     }
 
-    setIsPlayingMorse({ status: false })
+    setIsPlaying({ status: false })
   }
 
-  useEffect(() => { if (isPlayingMorse.status) playMorse() }, [isPlayingMorse])
+  useEffect(() => { if (isPlaying.status) play() }, [isPlaying])
 
-  const actionClickMorseButton = async () => {
-    if (isPlayingMorse.status) {
-      // HACK: so state inside playMorse function is mutated
-      isPlayingMorse.status = false
+  const actionClickPlayButton = async () => {
+    if (isPlaying.status) {
+      // HACK: so state inside play function is mutated
+      isPlaying.status = false
 
       if (dotData.isPlaying) dotData.stop()
       if (dashData.isPlaying) dashData.stop()
     } else {
-      setIsPlayingMorse({ status: true})
+      setIsPlaying({ status: true})
     }
   }
 
   return (
     <button
       tw="border text-white rounded-lg w-min px-6 py-2 mb-4 lg:mb-6 focus:(border-transparent ring-2 outline-none)"
-      css={[isPlayingMorse.status ? tw`bg-red-500 focus:ring-red-300` : tw`bg-blue-500 focus:ring-blue-300`]}
-      onClick={actionClickMorseButton}
+      css={[isPlaying.status ? tw`bg-red-500 focus:ring-red-300` : tw`bg-blue-500 focus:ring-blue-300`]}
+      onClick={actionClickPlayButton}
     >
-      { isPlayingMorse.status ? 'Berhenti' : 'Putar' }
+      { isPlaying.status
+        ? <div tw="flex items-center"><FaStop size="14" /><span tw="ml-2">Berhenti</span></div>
+        : <div tw="flex items-center"><FaPlay size="14" /><span tw="ml-2">Putar</span></div>
+      }
     </button>
   )
 }
