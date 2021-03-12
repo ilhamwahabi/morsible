30bf9346eb3b86be91028fd21c96807952a4e19e
iwgx <wahabiputra@gmail.com>
2021-03-12 21:54:44 +0700

    Add og and twitter meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,8 +17,33 @@ const App = ({ Component, pageProps }) => (
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name='description' content='Fast and Reliable Morse App' />
+      <meta name="keywords" content="text,morse,speech,translator,decoder,fast,reliable,iwgx,itu,english,indonesia,pwa" />
       <link rel="shortcut icon" href="favicon.ico" />
-      <title>Semar | Morse app</title>
+
+      {/* <!-- title --> */}
+      <title>Semar | Morse App</title>
+      <meta property='og:title' content='Semar | Morse App' />
+      <meta name='twitter:title' content='Semar | Morse App' />
+      
+      {/* <!-- url --> */}
+      <link rel='canonical' href='https://semar.iwgx.io' />
+      <meta property='og:url' content='https://semar.iwgx.io' />
+      <meta name='twitter:url' content='https://semar.iwgx.io' />
+      
+      {/* <!-- description --> */}
+      <meta name='description' content='Fast and reliable morse translator with speech functionality' />
+      <meta property='og:description' content='Fast and reliable morse translator with speech functionality' />
+      <meta name='twitter:description' content='Fast and reliable morse translator with speech functionality' />
+      
+      {/* <!-- image --> */}
+      <meta property="og:image" content="https://i.ibb.co/tY8jSh0/semar-card.png" />
+      <meta name="twitter:image" content="https://i.ibb.co/tY8jSh0/semar-card.png" />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:creator' content='@ilhamwahabigx' />
+
+      <meta property='og:type' content='website' />
+      <meta property='og:site_name' content='iwgx' />
     </Head>
     <GlobalStyles />
     <Component {...pageProps} />
