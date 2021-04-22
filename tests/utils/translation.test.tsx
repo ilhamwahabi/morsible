4aa014e4a966840e855f278845842932f64c2ea3
iwgx <wahabiputra@gmail.com>
2021-04-22 22:55:47 +0700

    Add test for getInvalidChar

--- tests/utils/translation.test.tsx
+++ tests/utils/translation.test.tsx
@@ -1,35 +1,45 @@
-import { textToMorse, morseToText } from '../../utils/translation';
+import { textToMorse, morseToText, getInvalidChar } from '../../utils/translation';
 
-const testCase = [
+const [case1, case2, case3] = [
   ['IF4081', '.. ..-. ....- ----- ---.. .----'],
   ['iwgx', '.. .-- --. -..-'],
   ['Semar - Morse translator', '... . -- .- .-. / -....- / -- --- .-. ... . / - .-. .- -. ... .-.. .- - --- .-.'],
 ]
 
 describe('translate text to morse correctly', () => {
-  it(`translate ${testCase[0][0]}`, () => {
-    expect(textToMorse(testCase[0][0])).toEqual(testCase[0][1])
+  it(`translate ${case1[0]}`, () => {
+    expect(textToMorse(case1[0])).toEqual(case1[1])
   })
 
-  it(`translate ${testCase[1][0]}`, () => {
-    expect(textToMorse(testCase[1][0])).toEqual(testCase[1][1])
+  it(`translate ${case2[0]}`, () => {
+    expect(textToMorse(case2[0])).toEqual(case2[1])
   })
 
-  it(`translate ${testCase[2][0]}`, () => {
-    expect(textToMorse(testCase[2][0])).toEqual(testCase[2][1])
+  it(`translate ${case3[0]}`, () => {
+    expect(textToMorse(case3[0])).toEqual(case3[1])
   })
 })
 
 describe('translate morse to text correctly', () => {
-  it(`translate ${testCase[0][1]}`, () => {
-    expect(morseToText(testCase[0][1])).toEqual(testCase[0][0].toLowerCase())
+  it(`translate ${case1[1]}`, () => {
+    expect(morseToText(case1[1])).toEqual(case1[0].toLowerCase())
   })
 
-  it(`translate ${testCase[1][1]}`, () => {
-    expect(morseToText(testCase[1][1])).toEqual(testCase[1][0].toLowerCase())
+  it(`translate ${case2[1]}`, () => {
+    expect(morseToText(case2[1])).toEqual(case2[0].toLowerCase())
   })
 
-  it(`translate ${testCase[2][1]}`, () => {
-    expect(morseToText(testCase[2][1])).toEqual(testCase[2][0].toLowerCase())
+  it(`translate ${case3[1]}`, () => {
+    expect(morseToText(case3[1])).toEqual(case3[0].toLowerCase())
+  })
+})
+
+describe('return array of invalid characters when any character invalid', () => {
+  it('return invalid characters', () => {
+    expect(getInvalidChar("^1 Bintang *")).toEqual(["^", "*"])
+  })
+
+  it('return empty array', () => {
+    expect(getInvalidChar("7u5t a n0rm4l t3xt")).toEqual([])
   })
 })
