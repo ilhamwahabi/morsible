9280f2e07871914ca843939ed3be16db4142b877
iwgx <wahabiputra@gmail.com>
2021-05-05 23:11:22 +0700

    Display notice when no speech, remove usePrevious

--- tests/hooks/usePrevious.test.tsx
+++
@@ -1,9 +0,0 @@
-import { renderHook } from '@testing-library/preact-hooks'
-
-import { usePrevious } from '../../hooks/usePrevious'
-
-test('should return undefined on initialized', () => {
-  const { result: previous } = renderHook(() => usePrevious("default value"))
-
-  expect(previous.current).toBe(undefined)
-})
